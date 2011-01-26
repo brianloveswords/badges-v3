1a48ad5431cefb5e3ceca4b8b6eefa6e17e01e17
Brian J Brennan <brianloveswords@gmail.com>
2011-01-26 15:28:01 -0500

    Using Mongo instead of CouchDB because I'm dumb and mongo is easier to do queries on for what I'm doing.

--- hub/config.ru
+++ hub/config.ru
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 require 'typhoeus'
+require 'mongo'
+require 'uuidtools'
 require 'hub.rb'
 
 run Hub
--- hub/hub.rb
+++ hub/hub.rb
@@ -1,26 +1,52 @@
 class Hub < Sinatra::Base
+  include UUIDTools
   use Rack::MethodOverride
+  
   # settings
   set :version, "0.1"
-  set :db_path, "http://127.0.0.1:5984"
+  
+  def initialize
+    @db = Mongo::Connection.new.db("badges")
+    super
+  end
   
   # routes
   get '/' do; "Hub #{settings.version}"; end
   
+  # register a new issuer
   post '/issuer/register' do
     path_to_identity = params['identity']
     secret_phrase = encrypt(params['phrase'])
     contents = JSON.parse(Typhoeus::Request.get(path_to_identity).body)
+    host = URI(path_to_identity).host
     
+    # bail if the secret phrase doesn't match
     unless contents['secret'] == secret_phrase
       headers = {'Content-Type' => 'text/plain'}
       body = {:error => "phrase_mismatch", "reason" => "The phrase submitted does not match the identity file."}.to_json
       return [403, headers, body]
     end
     
-    # contents['name']
     # store that sucker in the database
+    issuers = @db['issuers']
+    
+    # we shouldn't allow more than one entry per host
+    if issuers.find(:host => host).entries.length > 0
+      headers = {'Content-Type' => 'text/plain'}
+      body = {:error => "exists", "reason" => "This issuer is already registered"}.to_json
+      return [412, headers, body]
+    end
+    
+    doc = {:host => host, :name => contents['name'], :secret => contents['secret']}
+    res = issuers.insert(doc)
+    res.inspect
   end
+  
+  get '/db-test' do
+    issuers = @db['issuers']
+    UUID.random_create.to_s
+  end
+  
   
   protected
   def encrypt phrase

cd52bf2c6ff6cad8245a40a9304c0880b9dae8b6
Brian J Brennan <brianloveswords@gmail.com>
2011-01-26 11:57:59 -0500

    Providing identity data.

--- issuer/config.ru
+++ issuer/config.ru
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
 require 'issuer.rb'
 
 run Issuer
--- issuer/issuer.rb
+++ issuer/issuer.rb
@@ -4,8 +4,18 @@ class Issuer < Sinatra::Base
   set :version, "0.1"
   
   # routes
-  get '/' do
-    "Issuer #{settings.version}"
+  get '/' do; "Issuer #{settings.version}"; end
+
+  # Providing identity
+  get '/identity.json' do
+    {:name => 'Test Issuer', :secret => encrypt('awesometown')}.to_json
+  end
+
+  protected
+  def encrypt phrase
+    Digest::SHA2.new(256).update(phrase).to_s
   end
 end
+
+
 

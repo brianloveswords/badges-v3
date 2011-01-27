0caca16bbbc678a1f85d4056ca7956ea23083888
Brian J Brennan <brianloveswords@gmail.com>
2011-01-27 00:38:59 -0500

    Use some sessions instead of constantly reposting email address.

--- backpack/backpack.rb
+++ backpack/backpack.rb
@@ -1,26 +1,45 @@
 class Backpack < Sinatra::Base
   use Rack::MethodOverride
   set :version, "0.1"
+  enable :sessions
   
-  # version
+  # either present login form or show badges
   get '/' do
     @version = settings.version
-    haml :index
-  end
-
-  post '/' do
-    @version = settings.version
-    
-    @params = params
-    @badgesets = get_user_badges(params["email"])
-    
+    unless session['user']
+      return haml :index
+    end
+    @user = session['user']
+    @badgesets = get_user_badges(@user)
     haml :backpack
   end
 
+  # log a user out
+  get '/logout' do
+    session['user'] = nil
+    redirect '/'
+  end
+  
+  post '/' do
+    session['user'] = params['email']
+    redirect '/'
+  end
+
+  post '/update-privacy' do
+    email = session['user']
+    
+    # TODO: don't hardcode my own email address for one
+    headers = {"From" => email, "Authentication" => "this_is_a_password"}
+    uri = 'http://hub.rembr.it/user/update'
+    response = Typhoeus::Request.get(uri, :headers => headers, :params => {:badges => params[:badges] })
+    response.body
+  end
+  
+  
   protected
   def get_user_badges email
     headers = {"From" => email, "Authentication" => "this_is_a_password"}
-    badges = JSON.parse(Typhoeus::Request.get('http://hub.rembr.it/badges', :headers => headers).body)
+    badges = JSON.parse(Typhoeus::Request.get('http://hub.rembr.it/user/badges', :headers => headers).body)
   end
 end
 
--- backpack/public/js/backpack.js
+++ backpack/public/js/backpack.js
@@ -1,6 +1,45 @@
 (function(){
-  var badges = $('li.badge');
+  var badges = $('li.badge')
+  var containers = $('section')
+  var update_badges = function(){
+    badges = {
+      'private': [],
+      'public': [],
+      'rejected': []
+    }
+    containers.find('ul').each(function(){
+      var me = $(this)
+      var visibility = me.attr('rel')
+      me.find('li').each(function(){
+        badges[visibility].push($(this).attr('id'))
+      })
+    })
+    jQuery.post('/update-privacy', {badges: badges}, function(data){
+      console.log(data)
+    })
+  }
+  
   badges.bind('click', function(){
-    alert('you have clicked a badge. in the future this will do something')
+    console.log('clicked')
+  })
+  badges.draggable({
+    opacity: 0.65,
+    revert: "invalid",
+    revertDuration: 250,
+    helper: 'clone'
+  })
+  containers.droppable({
+    hoverClass: 'drophover',
+    drop: function(event, ui){
+      var parent = $(this).find('ul')
+      if (ui.draggable.parent() != parent) {
+        ui.draggable.fadeOut(250, function(){
+          parent.append(ui.draggable)
+          ui.draggable.fadeIn(250)
+          update_badges()
+        })
+        
+      }
+    }
   })
 })()
